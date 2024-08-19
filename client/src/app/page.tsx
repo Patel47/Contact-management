e4b9274b7d08f244ffcd228080a20a342211d6c1
Om Patel <omupatel7113@gmail.com>
2024-08-20 00:25:36 +0530

    setup shadcn

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>CONTACT MANAGEMENT</h1>
+      <Button>Click Me</Button>
     </main>
   );
 }

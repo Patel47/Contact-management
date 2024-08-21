c37a1ddcee3674ba5a5260b579ce87698d9cd4fb
Om Patel <omupatel7113@gmail.com>
2024-08-21 15:04:15 +0530

    add theme switcher

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="flex">
+            <div className="bg-red-400 w-[15%] ">
+              <Navbar />
+            </div>
+            <div className="p-4">{children}</div>
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <h1>CONTACT MANAGEMENT</h1>
-      <Button>Click Me</Button>
     </main>
   );
 }

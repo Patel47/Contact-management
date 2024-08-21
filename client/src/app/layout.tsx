f2ddc1b0b197b853cc8336b156da4bd8b6ff0c4b
Om Patel <omupatel7113@gmail.com>
2024-08-21 15:18:54 +0530

    remove class from html

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
+    <html lang="en" >
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
